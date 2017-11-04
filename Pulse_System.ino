c59548008eedbb3a63382a8c808e7070f40ab062
minmiguelm <cmiguelmg@gmail.com>
2017-11-04 16:54:41 -0500

    temporary connection to mqtt server

--- Pulse_System.ino
+++ Pulse_System.ino
@@ -1,45 +1,79 @@
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
 // Setup wifi connection
 const char* ssid = "AndroidAPM";
 const char* password = "1234567890";
 
+// MQTT Server
+const char* server = "192.168.43.186";
+const int port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 // Global variables
 int pulse = 0;
+
+// Constants
+const int blinkLed = D5;
+const int peak = 550;
 
 void setup_wifi() {
   delay(10);
   
-  Serial.println();
-  Serial.print("Conectando a ");
-  Serial.println(ssid);
-
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
+}
 
-  Serial.println("");
-  Serial.println("WiFi Conectado");
-  Serial.println("Dirección IP: ");
-  Serial.println(WiFi.localIP());
+void reconnect() {
+  while (!client.connected()) {
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str(),"disconnection",1,false,"")) {
+      client.subscribe("id-esp/pulse-sensor");
+    } else {
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  char rateStr[length];
+  for(int i = 0;i < length;i++)
+    rateStr[i] = (char)payload[i];
+  int rate = atoi(rateStr);
+  Serial.println(rate);
+  delay(10);
 }
 
 void setup() {
   Serial.begin(115200);
   pinMode(A0, INPUT);
-  pinMode(D5, OUTPUT);
-  //setup_wifi();
+  pinMode(blinkLed, OUTPUT);
+  setup_wifi();
+  client.setServer(server, port);
+  client.setCallback(callback);
 }
 
 void loop() {
-  pulse = analogRead(A0);
-  if(pulse >= 530)
-    digitalWrite(D5,HIGH);
-  else
-    digitalWrite(D5,LOW);
-  delay(50);
+  /*pulse = analogRead(A0);
   Serial.println(pulse);
+  if(pulse >= peak){
+    digitalWrite(blinkLed,HIGH);
+  }else
+    digitalWrite(blinkLed,LOW);
+  delay(10);*/
+  if (!client.connected()) {
+    reconnect();
+  } else {
+    pulse = analogRead(A0);
+    char str[4];
+    sprintf(str,"%d",pulse);
+    client.publish("id-esp/pulse-sensor",str);
+  }
+  client.loop();
 }
