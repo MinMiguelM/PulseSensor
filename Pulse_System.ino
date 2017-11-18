2e11dfc5a138c6c4348c166b9f566999cb742ef6
minmiguelm <cmiguelmg@gmail.com>
2017-11-17 20:15:12 -0500

    connection to temporal mqtt server. It's necessary to add a gateway for integrate with broker

--- Pulse_System.ino
+++ Pulse_System.ino
@@ -1,5 +1,11 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <Adafruit_SSD1306.h>
+
+#define UpperThreshold 550
+#define LowerThreshold 500
+
+#define OLED_Address 0x3C
 
 // Setup wifi connection
 const char* ssid = "AndroidAPM";
@@ -7,34 +13,48 @@ const char* password = "1234567890";
 
 // MQTT Server
 const char* server = "192.168.43.186";
+const char* token = "WUUYXIGsRChZP5SzFeFN";
 const int port = 1883;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+Adafruit_SSD1306 oled(3);
 
 // Global variables
-int pulse = 0;
+int x = 0;
+int lastX = 0;
+int lastY = 0;
+int lastTime = 0;
+bool BPMTiming = false;
+bool beatComplete = false;
+int BPM = 0;
 
 // Constants
-const int blinkLed = D5;
-const int peak = 550;
+const int pinBuzzer = 13;
 
 void setup_wifi() {
-  delay(10);
-  
   WiFi.begin(ssid, password);
-
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
+  
+  oled.clearDisplay();
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void reconnect() {
   while (!client.connected()) {
+    if ( WiFi.status() != WL_CONNECTED) {
+      oled.clearDisplay();
+      digitalWrite(LED_BUILTIN, HIGH);
+      setup_wifi();
+    }
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
-    if (client.connect(clientId.c_str(),"disconnection",1,false,"")) {
-      client.subscribe("id-esp/pulse-sensor");
+    if (client.connect(clientId.c_str(),token,NULL)) {
+      Serial.println("Connected to MQTT Server");
+      break;
     } else {
       delay(5000);
     }
@@ -52,28 +72,73 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 void setup() {
   Serial.begin(115200);
+  delay(100);
   pinMode(A0, INPUT);
-  pinMode(blinkLed, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  oled.begin(SSD1306_SWITCHCAPVCC, OLED_Address);
+  oled.setTextSize(2);
+  
   setup_wifi();
   client.setServer(server, port);
-  client.setCallback(callback);
+  //client.setCallback(callback);
 }
 
 void loop() {
-  /*pulse = analogRead(A0);
-  Serial.println(pulse);
-  if(pulse >= peak){
-    digitalWrite(blinkLed,HIGH);
-  }else
-    digitalWrite(blinkLed,LOW);
-  delay(10);*/
+  // Graph signal
+  if(x > 127){
+    oled.clearDisplay();
+    x = 0;
+    lastX = 0;
+  }
+
+  int pulse = analogRead(A0);
+  int now = millis();
+  int y = 60 - (pulse/16);
+  
+  oled.setTextColor(WHITE);
+  oled.writeLine(lastX,lastY,x,y,WHITE);
+  lastY=y;
+  lastX=x;
+  
+  if(pulse > UpperThreshold){
+    if(beatComplete){
+      BPM = now - lastTime;
+      BPM = int(60/(float(BPM)/1000));
+      BPMTiming = false;
+      beatComplete = false;
+
+      digitalWrite(pinBuzzer,HIGH);
+      delay(10);
+      digitalWrite(pinBuzzer,LOW);
+    }
+    if(BPMTiming == false){
+      lastTime = millis();
+      BPMTiming = true;
+    }
+  }
+  if((pulse < LowerThreshold) & (BPMTiming))
+    beatComplete = true;
+
+  // Graph BPM
+  oled.writeFillRect(0,50,128,16,BLACK);
+  oled.setCursor(0,50);
+  oled.print(BPM);
+  oled.print(" BPM");
+  oled.display();
+  x++;
   if (!client.connected()) {
     reconnect();
   } else {
-    pulse = analogRead(A0);
-    char str[4];
-    sprintf(str,"%d",pulse);
-    client.publish("id-esp/pulse-sensor",str);
+    String payload = "{";
+    payload += "\"signal\":"; payload += String(pulse); payload += ",";
+    payload += "\"BPM\":"; payload += String(BPM);
+    payload += "}";
+    char attributes[100];
+    payload.toCharArray( attributes, 100 );
+    Serial.println(payload);
+    client.publish("v1/devices/me/telemetry",attributes);
   }
   client.loop();
 }
